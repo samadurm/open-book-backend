e5d9b40d30f6a3f3be7ec0ba8e7f6b2bab8f7d3b
samadurm <samadurm@oregonstate.edu>
2020-12-18 18:53:08 -0800

    created basic CRUD routes for Course

--- Models/Course.cs
+++ Models/Course.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OpenBook.Models
 {    
@@ -9,7 +10,10 @@ namespace OpenBook.Models
         public List<long> Students { get; }
         public string Name { get; set; }
         public string Category { get; set; }
+        public string Description { get; set; }
         public int NumStudents { get; set; }
+        public List<short> Ratings { get; }
+        public float AverageRating { get; set; }
 
         public static string[] categories = {
             "Cooking",
@@ -22,14 +26,20 @@ namespace OpenBook.Models
             "Miscellaneous"
         };
 
-        public Course(string name, string category)
+        public Course(string name, string category, string description = "")
         {
             Name = name;
+
             Category = category;
             TeacherId = 0; 
 
+            Description = description;
+
             NumStudents = 0;
             Students = new List<long>();
+
+            Ratings = new List<short>();
+            AverageRating = 0.0F;
         }
 
         public void addStudent(long id)
@@ -44,6 +54,42 @@ namespace OpenBook.Models
             {
                 Students.Remove(id);    
                 NumStudents--;
+            } 
+            else
+            {
+                throw new System.Exception("Cannot remove student from empty students list.");
+            }
+        }
+
+        public float computeAverage()
+        {
+            if (Ratings.Count == 0)
+            {
+                AverageRating = 0.0F;
+            }
+            else 
+            {
+                float sum = 0.0F;
+
+                foreach (short rating in Ratings)
+                {
+                    sum += rating;
+                }
+                
+                AverageRating = sum / ((float)Ratings.Count);
+            }
+            return AverageRating;
+        }
+
+        public void addRating(short rating)
+        {
+            if (rating >= 0 && rating <= 5)
+            {
+                Ratings.Add(rating);
+                computeAverage();
+            } 
+            else {
+                throw new System.Exception("Rating out of range. Must enter in range 1-5.");
             }
         }
     }
