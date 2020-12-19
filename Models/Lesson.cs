c8323cadda9610da46dd5ecc91568464b1e4ea32
samadurm <samadurm@oregonstate.edu>
2020-12-19 15:03:39 -0800

    added lesson numbers so that frontend can sort them

--- Models/Lesson.cs
+++ Models/Lesson.cs
@@ -3,14 +3,16 @@ namespace OpenBook.Models
     public class Lesson
     {
         public long Id { get; set; }
+        public uint Number { get; set; }
         public string Title { get; set; }
         public long CourseId { get; set; }
         public string TextContent { get; set; }
 
-        public Lesson(string title, string textContent)
+        public Lesson(string title, string textContent, uint number)
         {
             Title = title;
             TextContent = textContent;
+            Number = number;
         }
     }
 }
