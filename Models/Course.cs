142e8e0a902af9079569c9a7368015f5fd066086
samadurm <samadurm@oregonstate.edu>
2020-12-18 16:32:02 -0800

    Added model for Course entity

---
+++ Models/Course.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+
+namespace OpenBook.Models
+{    
+    public class Course
+    {
+        public long Id { get; set; }
+        public long TeacherId { get; set; }
+        public List<long> Students { get; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int NumStudents { get; set; }
+
+        public static string[] categories = {
+            "Cooking",
+            "Technology",
+            "Programming",
+            "Science",
+            "Literature",
+            "Education",
+            "History",
+            "Miscellaneous"
+        };
+
+        public Course(string name, string category, long teacherId)
+        {
+            Name = name;
+            Category = category;
+            TeacherId = teacherId;
+
+            NumStudents = 0;
+            Students = new List<long>();
+        }
+
+        public void addStudent(long id)
+        {
+            Students.Add(id);
+            NumStudents++;
+        }
+
+        public void removeStudent(long id)
+        {
+            if (Students.Count > 0)
+            {
+                Students.Remove(id);    
+                NumStudents--;
+            }
+        }
+    }
+}
